8ce16e5949eab0a5e45daddf04a27757fc6b5ff3
Reema Rafeek <reemarafeek9@gmail.com>
2026-05-15 12:34:23 +0530

    code ui

---
+++ _com_namma_raste_health_NammaRasteApplication.java
@@ -0,0 +1,9 @@
+package dagger.hilt.internal.processedrootsentinel.codegen;
+
+import dagger.hilt.internal.processedrootsentinel.ProcessedRootSentinel;
+
+@ProcessedRootSentinel(
+    roots = "com.namma.raste.health.NammaRasteApplication"
+)
+public final class _com_namma_raste_health_NammaRasteApplication {
+}
